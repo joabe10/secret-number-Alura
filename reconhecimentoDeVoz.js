c7cf0bbfa50f0985f7b89105f0e8ae95b3dc58ea
Joabe <joabe.de.silva@gmail.com>
2023-08-10 15:07:30 -0300

    added voice recognition results to page using api and functions

--- reconhecimentoDeVoz.js
+++ reconhecimentoDeVoz.js
@@ -1,11 +1,29 @@
+const elementoChute = document.getElementById('chute')
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const recognition = new SpeechRecognition();
-recognition.lang = 'pt-Br'
+const recognition = new webkitSpeechRecognition();
+recognition.lang = 'pt-br'
 recognition.start()
 
 recognition.addEventListener('result', onSpeak)
 
 function onSpeak(e) {
-    console.log(e.results[0][0].transcript)
+    let chute = e.results[0][0].transcript
+    exibeElementoChute(chute)
+    
+}
+
+function exibeElementoChute(chute) {
+    elementoChute.innerHTML = `
+    <div>Você disse </div>
+    <span class="box">${chute}</span>
+    <div>O número secreto é ${maiorOuMenor()} <i class="fa-solid fa-up-long"></i></div>
+    `
+}
+
+function maiorOuMenor() {
+    if (numeroSecreto > chute)
+        return 'maior'
+    else
+        return 'menor'
 }
